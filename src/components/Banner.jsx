4f63d2ee8eaeb8cdc14102964eb7413e0d60f34f
minxng <minxng12@gmail.com>
2024-11-19 21:34:52 +0900

    [fix] 배너 이미지 서버 업로드

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,8 @@ export default function Banner() {
   return (
     <section className="relative w-full h-52 overflow-hidden">
       <img
-        className="w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-sm"
-        src={`${process.env.PUBLIC_URL}/image/bg.jpg`}
+        className="w-full absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-sm"
+        src={process.env.REACT_APP_BANNER_BACKGROUND_IMG}
         alt=""
       />
       <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">

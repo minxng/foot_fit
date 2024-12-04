3c09f6f156aae4ac475c66603f0b24ef74cc359f
minxng <minxng12@gmail.com>
2024-12-05 01:19:53 +0900

    [fix] 버튼 배경색 변경

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({ text, onClick, disabled }) {
+export default function Button({ text, onClick, disabled, bg }) {
   return (
     <button
-      className="bg-main text-white rounded px-4 py-2"
+      className={`${bg ? bg : "bg-main"} text-white rounded px-4 py-2`}
       onClick={onClick}
       disabled={disabled}
     >

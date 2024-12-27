9473f4ecb159aa03029c4299ace5624b4a71e051
minxng <minxng12@gmail.com>
2024-12-27 16:35:03 +0900

    [feature] 배너 추가

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,48 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 export default function Banner() {
+  const images = [
+    {
+      pc: "https://cdn11.bigcommerce.com/s-5e8c3uvulz/images/stencil/1300w/image-manager/spurs-2425away-shop-desktopcategoryhero-mic.jpg?t=1719825574",
+      mobile:
+        "https://cdn11.bigcommerce.com/s-5e8c3uvulz/images/stencil/480w/image-manager/spurs-2425away-shop-mobilecathero-sarr-6.jpg?t=1719825536",
+    },
+    {
+      pc: "https://res.cloudinary.com/dfd3hx72e/image/upload/v1735280627/banner_1_pga4my.jpg",
+      mobile:
+        "https://res.cloudinary.com/dfd3hx72e/image/upload/v1735284560/banner_1_mobile_dcbmo4.jpg",
+    },
+    {
+      pc: "https://res.cloudinary.com/dfd3hx72e/image/upload/v1735280552/banner_2_cvyubm.webp",
+      mobile:
+        "https://res.cloudinary.com/dfd3hx72e/image/upload/v1735284560/banner_2_mobile_nizo2n.webp",
+    },
+    {
+      pc: "https://res.cloudinary.com/dfd3hx72e/image/upload/v1735284560/banner_3_r3clqm.webp",
+      mobile:
+        "https://res.cloudinary.com/dfd3hx72e/image/upload/v1735284560/banner_3_mobile_bnfjea.webp",
+    },
+  ];
   return (
     <section className="w-full overflow-hidden">
-      <picture>
-        <source
-          srcSet="https://cdn11.bigcommerce.com/s-5e8c3uvulz/images/stencil/1300w/image-manager/spurs-2425away-shop-desktopcategoryhero-mic.jpg?t=1719825574"
-          media="(min-width: 640px)"
-        />
-        <img
-          className="w-full"
-          src="https://cdn11.bigcommerce.com/s-5e8c3uvulz/images/stencil/480w/image-manager/spurs-2425away-shop-mobilecathero-sarr-6.jpg?t=1719825536"
-          alt=""
-        />
-      </picture>
+      <Swiper
+        slidesPerView={1}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        loop={true}
+        modules={[Autoplay, Pagination, Navigation]}
+      >
+        {images.map((img) => (
+          <SwiperSlide key={img.pc}>
+            <picture>
+              <source srcSet={img.pc} media="(min-width: 640px)" />
+              <img className="w-full" src={img.mobile} alt="배너" />
+            </picture>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
